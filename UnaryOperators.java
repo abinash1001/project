280ac551121b5e5588a6d3fc849527ee07abbabf
M.Abinash <132438161+abinash1001@users.noreply.github.com>
2023-06-13 15:33:31 +0530

    Add files via upload

---
+++ UnaryOperators.java
@@ -0,0 +1,22 @@
+public class UnaryOperators
+
+ { 
+
+ public static void main(String[] args) 
+
+{
+
+ int i = 5; 
+
+ System.out.print(i++); 
+
+ System.out.print(++i); 
+
+ System.out.print(i--); 
+
+ System.out.print(--i);
+
+ } 
+
+}​
+
